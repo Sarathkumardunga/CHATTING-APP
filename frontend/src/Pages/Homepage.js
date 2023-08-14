66bb40edf67d5d05c47d1d461ab8a66b3d173d0b
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-14 23:49:57 +0530

    docs: started Login and signup pages

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Container, Box, Text, Tabs, Tab, TabList, TabPanels, TabPanel } from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
   return (
-    <div>Home</div>
-  )
+    <Container maxW={"xl"} centerContent>
+      <Box
+        d="flex"
+        justifyContent={"center"}
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Text
+          fontSize={"4xl"}
+          textAlign={"center"}
+          fontFamily="work sans"
+          fontWeight={"bold"}
+        >
+          Cool Chat
+        </Text>
+      </Box>
+
+      <Box
+        bg="white"
+        w={"100%"}
+        p={4}
+        borderRadius={"lg"}
+        color="black"
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab w={"50%"}>Login</Tab>
+            <Tab w={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel><Login /></TabPanel>
+            <TabPanel><Signup /></TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
 
 export default Homepage
