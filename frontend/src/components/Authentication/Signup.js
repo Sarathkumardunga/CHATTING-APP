8cf99337bd72465cc1b528715df53423c98b0fa7
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-16 23:26:30 +0530

    docs: Creaed some fields in Signup page

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import { VStack } from '@chakra-ui/layout'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [show, setShow] = useState(false);
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  
+  const handleClick = () => {
+    setShow(!show);
+  }
+
   return (
-    <div>Signup</div>
-  )
+    <VStack spacing="5px">
+      {/* Form space for name */}
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeHolder="Enter Your Name"
+          onChange={(e) => {
+            return setName(e.target.value);
+          }}
+        />
+      </FormControl>
+
+      {/* Form space for emailz */}
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email Address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      {/* Form space for password */}
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "hide" : "show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      {/* Form space for confirm password */}
+      <FormControl id="password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm password"
+            onChange={(e) => setConfirmpassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+    </VStack>
+  );
 }
 
 export default Signup
